02f643713686279ddfa1e119d0ef162d99d00ed0
João Keweni S R <joaokeweni49@gmail.com>
2026-04-27 10:39:13 -0300

    Criação do user.java

---
+++ api/src/main/java/com/taskflow/api/domain/entity/User.java
@@ -0,0 +1,20 @@
+package com.taskflow.api.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    // ... continue com email e password
+}
