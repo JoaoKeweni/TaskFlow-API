b8380187e28e5e06e2d35343097944a297f45a86
João Keweni S R <joaokeweni49@gmail.com>
2026-04-28 09:16:11 -0300

    feat: implement UserController with POST endpoint for user creation. My first result was "201:created"

---
+++ api/src/main/java/com/taskflow/api/controller/UserController.java
@@ -0,0 +1,35 @@
+package com.taskflow.api.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.taskflow.api.domain.entity.User;
+import com.taskflow.api.service.UserService;
+
+//@RestController define que a classe é um controlador REST
+@RestController
+// @RequestMapping define a URL base para todos os endpoints
+@RequestMapping("/api/users")
+
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        // @RequestBody avisa o Spring para pegar o JSON que veio na requisição
+        // e converter magicamente para o nosso objeto User.
+        // Passamos o pedido para o nosso "cozinheiro" (UserService)
+        User savedUser = userService.createUser(user);
+        // Devolvemos Status 201 (Created) junto com os dados do usuário recém salvo!
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedUser);
+    }
+
+}
