fce084df27c5afc866c8f585a3f1c6e2f86d002a
João Keweni S R <joaokeweni49@gmail.com>
2026-04-28 10:07:42 -0300

    Refactored methods to pass DTOs instead of my object.

--- api/src/main/java/com/taskflow/api/controller/UserController.java
+++ api/src/main/java/com/taskflow/api/controller/UserController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.taskflow.api.domain.dto.UserRequestDTO;
+import com.taskflow.api.domain.dto.UserResponseDTO;
 import com.taskflow.api.domain.entity.User;
 import com.taskflow.api.service.UserService;
 
@@ -23,11 +25,11 @@ public class UserController {
     }
 
     @PostMapping
-    public ResponseEntity<User> createUser(@RequestBody User user) {
+    public ResponseEntity<UserResponseDTO> createUser(@RequestBody UserRequestDTO dto) {
         // @RequestBody avisa o Spring para pegar o JSON que veio na requisição
         // e converter magicamente para o nosso objeto User.
         // Passamos o pedido para o nosso "cozinheiro" (UserService)
-        User savedUser = userService.createUser(user);
+        UserResponseDTO savedUser = userService.createUser(dto);
         // Devolvemos Status 201 (Created) junto com os dados do usuário recém salvo!
         return ResponseEntity.status(HttpStatus.CREATED).body(savedUser);
     }
--- api/src/main/java/com/taskflow/api/service/UserService.java
+++ api/src/main/java/com/taskflow/api/service/UserService.java
@@ -2,8 +2,10 @@ package com.taskflow.api.service;
 
 import org.springframework.stereotype.Service;
 
+import com.taskflow.api.domain.dto.UserRequestDTO;
 import com.taskflow.api.domain.entity.User;
 import com.taskflow.api.repository.UserRepository;
+import com.taskflow.api.domain.dto.UserResponseDTO;
 
 @Service
 public class UserService {
@@ -13,16 +15,27 @@ public class UserService {
         this.userRepository = userRepository;
     }
 
-    public User createUser(User user) {
+    public UserResponseDTO createUser(UserRequestDTO dto) {
         // TODO: regra pra conferir email existente(OK)
-        if (userRepository.existsByEmail(user.getEmail())) {
+        if (userRepository.existsByEmail(dto.email())) {
             throw new IllegalArgumentException("Este email já está cadatrado");
         }
         // TODO: regra pra conferir username existente(OK)
-        if (userRepository.existsByUsername(user.getUsername())) {
+        if (userRepository.existsByUsername(dto.username())) {
             throw new IllegalArgumentException("Este username já está em uso");
         }
-        return userRepository.save(user);
+
+        User user = new User();
+        user.setUsername(dto.username());
+        user.setEmail(dto.email());
+        user.setPassword(dto.password());
+
+        User savedUser = userRepository.save(user);
+
+        return new UserResponseDTO(
+                savedUser.getId(),
+                savedUser.getUsername(),
+                savedUser.getEmail());
     }
 
 }
