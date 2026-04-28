ea0bcf91d0495f9509f28eac00c61a064fbad99c
João Keweni S R <joaokeweni49@gmail.com>
2026-04-28 09:43:02 -0300

    add UserRequestDTO and UserResponseDTO classes for user data transfer

---
+++ api/src/main/java/com/taskflow/api/domain/dto/UserRequestDTO.java
@@ -0,0 +1,5 @@
+package com.taskflow.api.domain.dto;
+
+public record UserRequestDTO(String username, String email, String password) {
+
+}
---
+++ api/src/main/java/com/taskflow/api/domain/dto/UserResponseDTO.java
@@ -0,0 +1,4 @@
+package com.taskflow.api.domain.dto;
+
+public record UserResponseDTO(Long id, String username, String email) {
+}
